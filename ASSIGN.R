5194349a8f3c040e2c1bd8b6c25a30f17cc297c7
David Jenkins <d.jenkins0@gmail.com>
2014-12-16 21:44:33 -0500

    Improve comments and maximum 80 characters in each line

--- ASSIGN.R
+++ ASSIGN.R
@@ -1,34 +1,51 @@
-##Combat
+################################################################################
+#Input Files
+################################################################################
+#Input matrix with all test and training data
+rpkm_file <- "merged.rpkmlog"
+#Input phenotype text file with batch information and sample names
+pheno_file <- "pheno.txt"
+
+################################################################################
+#ComBat
+################################################################################
 library(sva)
 
 #Read in input data
-nonzerolog <-read.table("merged.rpkmlog", header=TRUE, row.names=1)
+nonzerolog <-read.table(rpkm_file, header=TRUE, row.names=1)
 
-#Read in pheno.txt file that contains phenotype information, example available in example_files/pheno.txt
-pheno <- read.table("pheno.txt", header=TRUE, row.names=1)
+#Read in pheno.txt file that contains phenotype information, example available
+#in example_files/pheno.txt
+pheno <- read.table(pheno_file, header=TRUE, row.names=1)
 
-#Create a model matrix for ComBat, just an intercept term because we are just adjusting for batch
+#Create a model matrix for ComBat, just an intercept term because we are just
+#adjusting for batch
 modcombat = model.matrix(~1, data=pheno)
 
-#Create an array of integers to indicate batches for the samples (modify the batch numbers to match your samples)
-batch <- c(rep(1,53),rep(2,192))
+#Batch information from pheno.txt file
+batch <- pheno$batch
 
 #Perform ComBat
-combat_rpkmlog <- ComBat(dat=nonzerolog, batch=batch, mod=modcombat, numCovs=NULL)
+combat_rpkmlog <- ComBat(dat=nonzerolog, batch=batch,
+                         mod=modcombat,  numCovs=NULL)
 
+################################################################################
 #ASSIGN
+################################################################################
 library(ASSIGN)
 #Create an output directory
 dir.create("output")
 tempdir <- "output"
 
-#Create a subset of the matrix containing only the training data (modify to match your samples)
+#Create a subset of the matrix containing only the training data (modify to
+#match your samples)
 sub_train <- combat_rpkmlog[, 1:53]
 
 #Create a list of labels for the training data in the way ASSIGN expects
 trainingLabel1 <- list(control = list(sig1=1:9,sig2=1:9), sig1=10:15,sig2=16:21)
 
-#Create a subset of the matrix containing only the test data (modify to match your samples)
+#Create a subset of the matrix containing only the test data (modify to match
+#your samples)
 sub_test_sample <- combat_rpkmlog[,54:245]
 
 #Create labels for the test data (modify to match your samples)
@@ -36,9 +53,10 @@ testLabel1 <- c(rep('tumor1',53),rep('tumor2',192))
 
 #ASSIGN - Preprocess
 processed.data <- assign.preprocess(trainingData=sub_train,
-                                            testData=sub_test_sample, 
-                                            trainingLabel=trainingLabel1, 
+                                            testData=sub_test_sample,
+                                            trainingLabel=trainingLabel1,
                                             geneList=NULL, n_sigGene=rep(200,5))
+
 #ASSIGN - MCMC, you may want to try adaptive_S as well
 mcmc.chain <- assign.mcmc(Y=processed.data$testData_sub,
                                   Bg = processed.data$B_vector,
@@ -46,14 +64,17 @@ mcmc.chain <- assign.mcmc(Y=processed.data$testData_sub,
                                   Delta_prior_p = processed.data$Pi_matrix,
                                   iter = 50000, adaptive_B=TRUE,
                                   adaptive_S=FALSE, mixture_beta=TRUE)
+
 #ASSIGN - Convergence
 trace.plot <- assign.convergence(test=mcmc.chain, burn_in=10000, iter=50000,
                                          parameter="B", whichGene=1,
                                          whichSample=NA, whichPath=NA)
+
 #ASSIGN - Summary
 mcmc.pos.mean <- assign.summary(test=mcmc.chain, burn_in=10000,
                                         iter=50000, adaptive_B=TRUE,
                                         adaptive_S=FALSE,mixture_beta=TRUE)
+
 #ASSIGN - Output
 assign.output(processed.data=processed.data,
               mcmc.pos.mean.testData=mcmc.pos.mean,
